4ea48bbd6f48b6b573cdb5779dd7d19b7164157a
Nevermore <15972699417@163.com>
2023-09-12 18:24:30 +0800

    更新图片显示

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,5 @@
 import process from 'node:process'
 import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
 import { defineUserConfig } from '@vuepress/cli'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
@@ -12,7 +11,6 @@ import { head, navbarEn, navbarZh, sidebarEn, sidebarZh,} from './configs/index'
 
 const __dirname = getDirname(import.meta.url)
 const isProd = process.env.NODE_ENV === 'production'
-console.log('process.env.DOCS_BUNDLER:', process.env.DOCS_BUNDLER)
 
 export default defineUserConfig({
   // set site base to default value
@@ -36,11 +34,11 @@ export default defineUserConfig({
   },
 
   // specify bundler via environment variable
-  bundler: process.env.DOCS_BUNDLER === 'webpack' ? webpackBundler() : viteBundler(),
+  bundler: viteBundler(),
 
   // configure default theme
   theme: defaultTheme({
-    logo: '/images/hero.png',
+    logo: '/images/yft-design.png',
     repo: 'vuepress/vuepress-next',
     docsDir: 'docs',
 
@@ -102,9 +100,6 @@ export default defineUserConfig({
       prismjs: !isProd,
     },
 
-    dev: {
-
-    }
   }),
 
   // configure markdown
@@ -116,66 +111,66 @@ export default defineUserConfig({
   },
 
   // use plugins
-  plugins: [
-    docsearchPlugin({
-      appId: '34YFD9IUQ2',
-      apiKey: '9a9058b8655746634e01071411c366b8',
-      indexName: 'vuepress',
-      searchParameters: {
-        facetFilters: ['tags:v2'],
-      },
-      locales: {
-        '/zh/': {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档',
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消',
-              },
-              startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除',
-              },
-              errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接',
-              },
-              footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者',
-              },
-              noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈',
-              },
-            },
-          },
-        },
-      },
-    }),
-    googleAnalyticsPlugin({
-      // we have multiple deployments, which would use different id
-      id: process.env.DOCS_GA_ID ?? '',
-    }),
-    registerComponentsPlugin({
-      componentsDir: path.resolve(__dirname, './components'),
-    }),
-    // only enable shiki plugin in production mode
-    isProd ? shikiPlugin({ theme: 'dark-plus' }) : [],
-  ],
+  // plugins: [
+  //   docsearchPlugin({
+  //     appId: '34YFD9IUQ2',
+  //     apiKey: '9a9058b8655746634e01071411c366b8',
+  //     indexName: 'vuepress',
+  //     searchParameters: {
+  //       facetFilters: ['tags:v2'],
+  //     },
+  //     locales: {
+  //       '/zh/': {
+  //         placeholder: '搜索文档',
+  //         translations: {
+  //           button: {
+  //             buttonText: '搜索文档',
+  //             buttonAriaLabel: '搜索文档',
+  //           },
+  //           modal: {
+  //             searchBox: {
+  //               resetButtonTitle: '清除查询条件',
+  //               resetButtonAriaLabel: '清除查询条件',
+  //               cancelButtonText: '取消',
+  //               cancelButtonAriaLabel: '取消',
+  //             },
+  //             startScreen: {
+  //               recentSearchesTitle: '搜索历史',
+  //               noRecentSearchesText: '没有搜索历史',
+  //               saveRecentSearchButtonTitle: '保存至搜索历史',
+  //               removeRecentSearchButtonTitle: '从搜索历史中移除',
+  //               favoriteSearchesTitle: '收藏',
+  //               removeFavoriteSearchButtonTitle: '从收藏中移除',
+  //             },
+  //             errorScreen: {
+  //               titleText: '无法获取结果',
+  //               helpText: '你可能需要检查你的网络连接',
+  //             },
+  //             footer: {
+  //               selectText: '选择',
+  //               navigateText: '切换',
+  //               closeText: '关闭',
+  //               searchByText: '搜索提供者',
+  //             },
+  //             noResultsScreen: {
+  //               noResultsText: '无法找到相关结果',
+  //               suggestedQueryText: '你可以尝试查询',
+  //               reportMissingResultsText: '你认为该查询应该有结果？',
+  //               reportMissingResultsLinkText: '点击反馈',
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   }),
+  //   googleAnalyticsPlugin({
+  //     // we have multiple deployments, which would use different id
+  //     id: process.env.DOCS_GA_ID ?? '',
+  //   }),
+  //   registerComponentsPlugin({
+  //     componentsDir: path.resolve(__dirname, './components'),
+  //   }),
+  //   // only enable shiki plugin in production mode
+  //   isProd ? shikiPlugin({ theme: 'dark-plus' }) : [],
+  // ],
 })
